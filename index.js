f307bcf6a91072909016fb816f709b530ea24493
mrgunter51 <mrgunter@crimson.ua.edu>
2017-10-10 08:15:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine,name) {
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length + 1} in line.`
+}
+
+function nowServing(array) {
+  if(array.length === 0){
+    return "There is nobody waiting to be served!"
+  }
+  else{
+    return array.pop()
+  }
+}
+
+// function currentLine(array){
+//   if(array.length === 0){
+//     return "The line is currently empty."
+//   }
+//   else{
+//     for(let i = 0;i < array.length;i++){
+//
+//     }
+//   }
+// }
